89246848dc96d43612c96ffbc73e9dbe8b9d558e
Curve <fynnbwdt@gmail.com>
2021-11-26 19:51:12 +0100

    feat(windows): update CMakeLists.txt

---
+++ NuGet.cmake
@@ -0,0 +1,14 @@
+function(nuget_add OUTPUT_NAME NAME VERSION)
+    find_program(NUGET nuget)
+
+    if (NUGET-NOTFOUND)
+        if (NOT EXISTS "nuget-cli.exe")
+            message(STATUS "[NuGet] NuGet-CLI not found, downloading...")
+            file(DOWNLOAD "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" "${CMAKE_CURRENT_BINARY_DIR}/tools/nuget-cli.exe")
+            set(NUGET "${CMAKE_CURRENT_BINARY_DIR}/tools/nuget-cli.exe")
+        endif()
+    endif()
+
+    execute_process(COMMAND ${NUGET} install ${NAME} -Version ${VERSION} -ExcludeVersion -OutputDirectory ${CMAKE_CURRENT_BINARY_DIR}/packages OUTPUT_QUIET COMMAND_ERROR_IS_FATAL ANY)
+    set(${OUTPUT_NAME}_PATH "${CMAKE_CURRENT_BINARY_DIR}/packages/${NAME}" PARENT_SCOPE)
+endfunction()
