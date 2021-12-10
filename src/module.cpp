ac8605eb8316cc0285fdb659ea3b9c1c0c746774
Curve <fynnbwdt@gmail.com>
2021-12-10 15:41:21 +0100

    refactor: revert 65bd3d7, b244eda, 6b70f52, 10f360f

--- src/module.cpp
+++
@@ -1,9 +0,0 @@
-#include <module/module.hpp>
-
-namespace saucer
-{
-    void module::on_url_changed(const std::string &) {}
-
-    module::~module() = default;
-    module::module(webview &webview) : m_webview(webview) {}
-} // namespace saucer
