98da911e805af770ce3b17a16719e490c580bbe3
Curve <fynnbwdt@gmail.com>
2021-12-17 18:18:29 +0100

    fix(nuget.cmake): existance check

--- NuGet.cmake
+++ NuGet.cmake
@@ -2,11 +2,11 @@ function(nuget_add OUTPUT_NAME NAME VERSION)
     find_program(NUGET nuget)
 
     if (NUGET-NOTFOUND)
-        if (NOT EXISTS "nuget-cli.exe")
+        if (NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/tools/nuget-cli.exe")
             message(STATUS "[NuGet] NuGet-CLI not found, downloading...")
             file(DOWNLOAD "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" "${CMAKE_CURRENT_BINARY_DIR}/tools/nuget-cli.exe")
-            set(NUGET "${CMAKE_CURRENT_BINARY_DIR}/tools/nuget-cli.exe")
         endif()
+        set(NUGET "${CMAKE_CURRENT_BINARY_DIR}/tools/nuget-cli.exe")
     endif()
 
     execute_process(COMMAND ${NUGET} install ${NAME} -Version ${VERSION} -ExcludeVersion -OutputDirectory ${CMAKE_CURRENT_BINARY_DIR}/packages OUTPUT_QUIET COMMAND_ERROR_IS_FATAL ANY)
