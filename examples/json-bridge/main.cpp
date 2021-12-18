78e86e35af78d12108f774e374e6feacf8f7be64
Curve <fynnbwdt@gmail.com>
2021-12-18 16:50:00 +0100

    refactor(json-example): update example

--- examples/json-bridge/main.cpp
+++ examples/json-bridge/main.cpp
@@ -1,10 +1,10 @@
-#include <bridge/bridged_webview.hpp>
-#include <bridge/json/json_bridge.hpp>
-#include <bridge/json/promise.hpp>
 #include <iostream>
+#include <serializers/json.hpp>
+#include <smartview.hpp>
+
 int main()
 {
-    saucer::bridged_webview<saucer::bridges::json> webview;
+    saucer::simple_smartview<saucer::serializers::json> webview;
 
     webview.set_title("Hello World!");
     webview.set_size(500, 600);
@@ -15,11 +15,9 @@ int main()
     webview.on_resize([&](std::size_t width, std::size_t height) { std::cout << width << ":" << height << std::endl; });
     webview.on_url_changed([](const std::string &url) { std::cout << "New url:" << url << std::endl; });
 
-    webview.call<float>("Math.random")->then([](float result) { std::cout << "Random: " << result << std::endl; });
-    webview.call<int>("Math.pow", 5, 2)->then([](int result) { std::cout << "Pow(5,2): " << result << std::endl; });
-    webview.call<int>("await window.saucer.call", "test", std::make_tuple(10, "Test", 5.f))->then([](int result) {
-        std::cout << "Test result: " << result << std::endl;
-    });
+    webview.eval<float>("Math.random()")->then([](float result) { std::cout << "Random: " << result << std::endl; });
+    webview.eval<int>("Math.pow({},{})", 5, 2)->then([](int result) { std::cout << "Pow(5,2): " << result << std::endl; });
+    webview.eval<int>("await window.saucer.call({}, {})", "test", std::make_tuple(10, "Test", 5.f))->then([](int result) { std::cout << "Test result: " << result << std::endl; });
 
     webview.expose("test", [](int _int, const std::string &_string, float _float) {
         std::cout << "Int: " << _int << ", "
@@ -28,8 +26,7 @@ int main()
         return 1337;
     });
 
-    webview.set_url("https://google.com");
-
+    webview.set_url("file://test.html");
     webview.show();
     webview.run();
 
