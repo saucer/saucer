c0c856b0333c1632740a92f65f1d54b1551ea1d6
Curve <fynnbwdt@gmail.com>
2022-08-23 13:47:52 +0200

    refactor(cli-embedding-example): demonstrate embedding with nested folder structure

--- examples/cli-embedding/embedding/all.hpp
+++ examples/cli-embedding/embedding/all.hpp
@@ -4,18 +4,18 @@
 #include <string>
 #include <saucer/webview.hpp>
 
-#include "logo.png.hpp"
-#include "index.js.hpp"
-#include "index.html.hpp"
-#include "style.css.hpp"
+#include "style/style.css.hpp"
+#include "assets/logo.png.hpp"
+#include "src/index.js.hpp"
+#include "src/index.html.hpp"
 
 namespace embedded {
 	inline auto get_all_files() {
 		std::map<const std::string, const saucer::embedded_file> rtn;
-		rtn.emplace("logo.png", saucer::embedded_file{"image/png", 133033, embedded_logo_png});
-		rtn.emplace("index.js", saucer::embedded_file{"application/javascript", 150, embedded_index_js});
-		rtn.emplace("index.html", saucer::embedded_file{"text/html", 299, embedded_index_html});
-		rtn.emplace("style.css", saucer::embedded_file{"text/css", 97, embedded_style_css});
+		rtn.emplace("style/style.css", saucer::embedded_file{"text/css", 97, embedded_style_style_css});
+		rtn.emplace("assets/logo.png", saucer::embedded_file{"image/png", 133033, embedded_assets_logo_png});
+		rtn.emplace("src/index.js", saucer::embedded_file{"application/javascript", 150, embedded_src_index_js});
+		rtn.emplace("src/index.html", saucer::embedded_file{"text/html", 318, embedded_src_index_html});
 		return rtn;
 	}
 } // namespace embedded
