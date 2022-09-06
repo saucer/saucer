104aa3ec4e6a401009d0b656de658a60e150db9e
Curve <fynnbwdt@gmail.com>
2022-09-06 10:30:03 +1000

    refactor(ffi): use cmake to export symbols

--- shared/exports.cpp
+++ shared/exports.cpp
@@ -4,58 +4,52 @@
 #include <saucer/webview.hpp>
 #include <saucer/smartview.hpp>
 
-#if defined(_WIN32)
-#define extern __export __declspec(dllexport)
-#else
-#define __export extern __attribute__((visibility("default")))
-#endif
-
 #define requires_free
 
 extern "C"
 {
-    __export bool window_get_resizable(const saucer::window *window)
+    bool window_get_resizable(const saucer::window *window)
     {
         return window->get_resizable();
     }
 
-    __export void window_get_title(const saucer::window *window, char *output, size_t output_size)
+    void window_get_title(const saucer::window *window, char *output, size_t output_size)
     {
         strcpy(output, window->get_title().substr(0, output_size).c_str());
     }
 
-    __export bool window_get_decorations(const saucer::window *window)
+    bool window_get_decorations(const saucer::window *window)
     {
         return window->get_decorations();
     }
 
-    __export bool window_get_always_on_top(const saucer::window *window)
+    bool window_get_always_on_top(const saucer::window *window)
     {
         return window->get_always_on_top();
     }
 
-    __export void window_get_size(const saucer::window *window, size_t *width, size_t *height)
+    void window_get_size(const saucer::window *window, size_t *width, size_t *height)
     {
         auto [w, h] = window->get_size();
         *width = w;
         *height = h;
     }
 
-    __export void window_get_max_size(const saucer::window *window, size_t *width, size_t *height)
+    void window_get_max_size(const saucer::window *window, size_t *width, size_t *height)
     {
         auto [w, h] = window->get_max_size();
         *width = w;
         *height = h;
     }
 
-    __export void window_get_min_size(const saucer::window *window, size_t *width, size_t *height)
+    void window_get_min_size(const saucer::window *window, size_t *width, size_t *height)
     {
         auto [w, h] = window->get_min_size();
         *width = w;
         *height = h;
     }
 
-    __export void window_get_background_color(const saucer::window *window, size_t *r, size_t *g, size_t *b, size_t *a)
+    void window_get_background_color(const saucer::window *window, size_t *r, size_t *g, size_t *b, size_t *a)
     {
         auto [_r, _g, _b, _a] = window->get_background_color();
         *r = _r;
@@ -64,57 +58,57 @@ extern "C"
         *a = _a;
     }
 
-    __export void window_hide(saucer::window *window)
+    void window_hide(saucer::window *window)
     {
         window->hide();
     }
 
-    __export void window_show(saucer::window *window)
+    void window_show(saucer::window *window)
     {
         window->show();
     }
 
-    __export void window_close(saucer::window *window)
+    void window_close(saucer::window *window)
     {
         window->close();
     }
 
-    __export void window_set_resizable(saucer::window *window, bool enabled)
+    void window_set_resizable(saucer::window *window, bool enabled)
     {
         window->set_resizable(enabled);
     }
 
-    __export void window_set_title(saucer::window *window, const char *title)
+    void window_set_title(saucer::window *window, const char *title)
     {
         window->set_title(title);
     }
 
-    __export void window_set_decorations(saucer::window *window, bool enabled)
+    void window_set_decorations(saucer::window *window, bool enabled)
     {
         window->set_decorations(enabled);
     }
 
-    __export void window_set_always_on_top(saucer::window *window, bool enabled)
+    void window_set_always_on_top(saucer::window *window, bool enabled)
     {
         window->set_always_on_top(enabled);
     }
 
-    __export void window_set_size(saucer::window *window, size_t width, size_t height)
+    void window_set_size(saucer::window *window, size_t width, size_t height)
     {
         window->set_size(width, height);
     }
 
-    __export void window_set_max_size(saucer::window *window, size_t width, size_t height)
+    void window_set_max_size(saucer::window *window, size_t width, size_t height)
     {
         window->set_max_size(width, height);
     }
 
-    __export void window_set_min_size(saucer::window *window, size_t width, size_t height)
+    void window_set_min_size(saucer::window *window, size_t width, size_t height)
     {
         window->set_min_size(width, height);
     }
 
-    __export void window_set_background_color(saucer::window *window, size_t r, size_t g, size_t b, size_t a)
+    void window_set_background_color(saucer::window *window, size_t r, size_t g, size_t b, size_t a)
     {
         window->set_background_color(r, g, b, a);
     }
@@ -125,27 +119,27 @@ extern "C"
         close
     };
 
-    __export void window_clear(saucer::window *window, window_event event)
+    void window_clear(saucer::window *window, window_event event)
     {
         window->clear(static_cast<saucer::window_event>(event));
     }
 
-    __export void window_remove(saucer::window *window, window_event event, uint64_t id)
+    void window_remove(saucer::window *window, window_event event, uint64_t id)
     {
         window->remove(static_cast<saucer::window_event>(event), id);
     }
 
-    __export uint64_t window_on_resize(saucer::window *window, void (*callback)(size_t, size_t))
+    uint64_t window_on_resize(saucer::window *window, void (*callback)(size_t, size_t))
     {
         return window->on<saucer::window_event::resize>(callback);
     }
 
-    __export uint64_t window_on_close(saucer::window *window, bool (*callback)())
+    uint64_t window_on_close(saucer::window *window, bool (*callback)())
     {
         return window->on<saucer::window_event::close>(callback);
     }
 
-    __export void window_run()
+    void window_run()
     {
         saucer::window::run();
     }
@@ -153,57 +147,57 @@ extern "C"
 
 extern "C"
 {
-    __export bool webview_get_dev_tools(const saucer::webview *webview)
+    bool webview_get_dev_tools(const saucer::webview *webview)
     {
         return webview->get_dev_tools();
     }
 
-    __export void webview_get_url(const saucer::webview *webview, char *output, size_t output_size)
+    void webview_get_url(const saucer::webview *webview, char *output, size_t output_size)
     {
         strcpy(output, webview->get_url().substr(0, output_size).c_str());
     }
 
-    __export bool webview_get_transparent(const saucer::webview *webview)
+    bool webview_get_transparent(const saucer::webview *webview)
     {
         return webview->get_transparent();
     }
 
-    __export bool webview_get_context_menu(const saucer::webview *webview)
+    bool webview_get_context_menu(const saucer::webview *webview)
     {
         return webview->get_context_menu();
     }
 
-    __export void webview_serve_embedded(saucer::webview *webview, const char *file)
+    void webview_serve_embedded(saucer::webview *webview, const char *file)
     {
         webview->serve_embedded(file);
     }
 
-    __export void webview_set_dev_tools(saucer::webview *webview, bool enabled)
+    void webview_set_dev_tools(saucer::webview *webview, bool enabled)
     {
         webview->set_dev_tools(enabled);
     }
 
-    __export void webview_set_context_menu(saucer::webview *webview, bool enabled)
+    void webview_set_context_menu(saucer::webview *webview, bool enabled)
     {
         webview->set_context_menu(enabled);
     }
 
-    __export void webview_set_url(saucer::webview *webview, const char *url)
+    void webview_set_url(saucer::webview *webview, const char *url)
     {
         webview->set_url(url);
     }
 
-    [[requires_free]] __export std::pair<std::string, saucer::embedded_file> *embedded_file_new(const char *name, const char *mime, std::size_t size, const std::uint8_t *data)
+    [[requires_free]] std::pair<std::string, saucer::embedded_file> *embedded_file_new(const char *name, const char *mime, std::size_t size, const std::uint8_t *data)
     {
         return new std::pair<std::string, saucer::embedded_file>(name, {mime, size, data});
     }
 
-    __export void embedded_file_free(saucer::embedded_file *file)
+    void embedded_file_free(saucer::embedded_file *file)
     {
         delete file;
     }
 
-    __export void webview_embed_files(saucer::webview *webview, std::pair<std::string, saucer::embedded_file> **files, std::size_t file_count)
+    void webview_embed_files(saucer::webview *webview, std::pair<std::string, saucer::embedded_file> **files, std::size_t file_count)
     {
         std::map<const std::string, const saucer::embedded_file> embedded_files;
 
@@ -216,22 +210,22 @@ extern "C"
         webview->embed_files(std::move(embedded_files));
     }
 
-    __export void webview_set_transparent(saucer::webview *webview, bool enabled, bool blur = false)
+    void webview_set_transparent(saucer::webview *webview, bool enabled, bool blur = false)
     {
         webview->set_transparent(enabled, blur);
     }
 
-    __export void webview_clear_scripts(saucer::webview *webview)
+    void webview_clear_scripts(saucer::webview *webview)
     {
         webview->clear_scripts();
     }
 
-    __export void webview_clear_embedded(saucer::webview *webview)
+    void webview_clear_embedded(saucer::webview *webview)
     {
         webview->clear_embedded();
     }
 
-    __export void webview_run_java_script(saucer::webview *webview, const char *java_script)
+    void webview_run_java_script(saucer::webview *webview, const char *java_script)
     {
         webview->run_java_script(java_script);
     }
@@ -242,7 +236,7 @@ extern "C"
         creation,
     };
 
-    __export void webview_inject(saucer::webview *webview, const char *java_script, load_time load_time)
+    void webview_inject(saucer::webview *webview, const char *java_script, load_time load_time)
     {
         webview->inject(java_script, static_cast<saucer::load_time>(load_time));
     }
@@ -252,17 +246,17 @@ extern "C"
         url_changed
     };
 
-    __export void webview_clear(saucer::webview *webview, web_event event)
+    void webview_clear(saucer::webview *webview, web_event event)
     {
         webview->clear(static_cast<saucer::web_event>(event));
     }
 
-    __export void webview_remove(saucer::webview *webview, web_event event, uint64_t id)
+    void webview_remove(saucer::webview *webview, web_event event, uint64_t id)
     {
         webview->remove(static_cast<saucer::web_event>(event), id);
     }
 
-    __export uint64_t webview_on_url_changed(saucer::webview *webview, void (*callback)(const char *))
+    uint64_t webview_on_url_changed(saucer::webview *webview, void (*callback)(const char *))
     {
         return webview->on<saucer::web_event::url_changed>([callback](const std::string &url) { callback(url.c_str()); });
     }
@@ -270,7 +264,7 @@ extern "C"
 
 extern "C"
 {
-    __export saucer::ffi_function_data *ffi_function_data_new(std::size_t id, const char *function, void *data)
+    saucer::ffi_function_data *ffi_function_data_new(std::size_t id, const char *function, void *data)
     {
         auto *rtn = new saucer::ffi_function_data;
         rtn->function = function;
@@ -280,22 +274,22 @@ extern "C"
         return rtn;
     }
 
-    __export void ffi_function_data_get_function(saucer::ffi_function_data *data, char *output, std::size_t output_size)
+    void ffi_function_data_get_function(saucer::ffi_function_data *data, char *output, std::size_t output_size)
     {
         strcpy(output, data->function.substr(0, output_size).c_str());
     }
 
-    __export void *ffi_function_data_get_data(saucer::ffi_function_data *data)
+    void *ffi_function_data_get_data(saucer::ffi_function_data *data)
     {
         return data->data;
     }
 
-    __export std::size_t ffi_function_data_get_id(saucer::ffi_function_data *data)
+    std::size_t ffi_function_data_get_id(saucer::ffi_function_data *data)
     {
         return data->id;
     }
 
-    __export saucer::ffi_result_data *ffi_result_data_new(std::size_t id, void *data)
+    saucer::ffi_result_data *ffi_result_data_new(std::size_t id, void *data)
     {
         auto *rtn = new saucer::ffi_result_data;
         rtn->data = data;
@@ -304,37 +298,37 @@ extern "C"
         return rtn;
     }
 
-    __export void *ffi_result_data_get_data(saucer::ffi_result_data *data)
+    void *ffi_result_data_get_data(saucer::ffi_result_data *data)
     {
         return data->data;
     }
 
-    __export std::size_t ffi_result_data_get_id(saucer::ffi_result_data *data)
+    std::size_t ffi_result_data_get_id(saucer::ffi_result_data *data)
     {
         return data->id;
     }
 
-    __export void ffi_serializer_set_buffer_size(std::size_t size)
+    void ffi_serializer_set_buffer_size(std::size_t size)
     {
         saucer::ffi_serializer::buffer_size = size;
     }
 
-    __export void ffi_serializer_set_init_script(const char *script)
+    void ffi_serializer_set_init_script(const char *script)
     {
         saucer::ffi_serializer::init_script = script;
     }
 
-    __export void ffi_serializer_set_js_serializer(const char *serializer)
+    void ffi_serializer_set_js_serializer(const char *serializer)
     {
         saucer::ffi_serializer::js_serializer = serializer;
     }
 
-    __export void ffi_serializer_set_parse_callback(saucer::ffi_serializer::parse_callback_t callback)
+    void ffi_serializer_set_parse_callback(saucer::ffi_serializer::parse_callback_t callback)
     {
         saucer::ffi_serializer::parse_callback = callback;
     }
 
-    __export void ffi_serializer_set_serialize_callback(saucer::ffi_serializer::serialize_callback_t callback)
+    void ffi_serializer_set_serialize_callback(saucer::ffi_serializer::serialize_callback_t callback)
     {
         saucer::ffi_serializer::serialize_callback = callback;
     }
@@ -342,17 +336,17 @@ extern "C"
 
 extern "C"
 {
-    [[requires_free]] __export saucer::smartview *smartview_new()
+    [[requires_free]] saucer::smartview *smartview_new()
     {
         return new saucer::smartview;
     }
 
-    __export void smartview_free(saucer::smartview *smartview)
+    void smartview_free(saucer::smartview *smartview)
     {
         delete smartview;
     }
 
-    __export void smartview_expose(saucer::smartview *smartview, const char *name, bool async)
+    void smartview_expose(saucer::smartview *smartview, const char *name, bool async)
     {
         smartview->expose<saucer::ffi_serializer>(name, smartview, async);
     }
