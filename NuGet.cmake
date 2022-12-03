9faaa403888e30e3f58e043b6765ff2183587bbd
Curve <fynnbwdt@gmail.com>
2022-12-03 13:59:34 -0800

    feat(nuget): check for sources, print additional info

--- NuGet.cmake
+++ NuGet.cmake
@@ -1,6 +1,8 @@
 function(nuget_add OUTPUT_NAME NAME VERSION)
     find_program(NUGET nuget)
 
+    message(STATUS "[NuGet] Downloading Package: ${NAME} [${VERSION}]")
+
     if (NUGET STREQUAL "NUGET-NOTFOUND")
         if (NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/tools/nuget-cli.exe")
             message(STATUS "[NuGet] NuGet-CLI not found, downloading...")
@@ -8,7 +10,18 @@ function(nuget_add OUTPUT_NAME NAME VERSION)
         endif()
         set(NUGET "${CMAKE_CURRENT_BINARY_DIR}/tools/nuget-cli.exe")
     endif()
+    
+    execute_process(COMMAND ${NUGET} help OUTPUT_VARIABLE NUGET_INFO)
+    string(REGEX MATCH "NuGet Version: ([0-9.]+)" _ "${NUGET_INFO}")
+    message(STATUS "[NuGet] Found at: ${NUGET} [${CMAKE_MATCH_1}]")
 
-    execute_process(COMMAND ${NUGET} install ${NAME} -Version ${VERSION} -ExcludeVersion -OutputDirectory ${CMAKE_CURRENT_BINARY_DIR}/packages OUTPUT_QUIET COMMAND_ERROR_IS_FATAL ANY)
+    execute_process(COMMAND ${NUGET} sources OUTPUT_VARIABLE NUGET_SOURCES)
+    
+    if (NOT "${NUGET_SOURCES}" MATCHES "https:\/\/api\.nuget\.org\/v3\/index\.json")
+        message(WARNING "[NuGet] Official NuGet Sources missing!")
+        execute_process(COMMAND ${NUGET} sources Add -Name "nuget.org" -Source "https://api.nuget.org/v3/index.json")
+    endif()
+
+    execute_process(COMMAND ${NUGET} install ${NAME} -Version ${VERSION} -ExcludeVersion -OutputDirectory "${CMAKE_CURRENT_BINARY_DIR}/packages" OUTPUT_QUIET COMMAND_ERROR_IS_FATAL ANY)
     set(${OUTPUT_NAME}_PATH "${CMAKE_CURRENT_BINARY_DIR}/packages/${NAME}" PARENT_SCOPE)
 endfunction()
