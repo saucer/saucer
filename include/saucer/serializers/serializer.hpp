b7b11644d2c91a748f322c9468b334039bf84f40
Curve <fynnbwdt@gmail.com>
2023-09-09 18:37:10 +0200

    refactor(serializer): adjust alias names

--- include/saucer/serializers/serializer.hpp
+++ include/saucer/serializers/serializer.hpp
@@ -14,16 +14,17 @@
 
 namespace saucer
 {
-    enum class serializer_error
+    enum class serializer_error : std::uint8_t
     {
-        argument_count_mismatch,
         type_mismatch,
+        invalid_function,
+        argument_count_mismatch,
     };
 
     struct serializer
     {
-        using message_ptr = std::unique_ptr<message_data>;
-        using promise = std::function<void(result_data &)>;
+        using parse_result = std::unique_ptr<message_data>;
+        using resolver = std::function<void(result_data &)>;
         using function = std::function<tl::expected<std::string, serializer_error>(function_data &)>;
 
       public:
@@ -34,11 +35,11 @@ namespace saucer
         [[nodiscard]] virtual std::string js_serializer() const = 0;
 
       public:
-        [[nodiscard]] virtual message_ptr parse(const std::string &) const = 0;
+        [[nodiscard]] virtual parse_result parse(const std::string &) const = 0;
     };
 
     template <class T>
-    concept Serializer = requires() {
+    concept Serializer = requires {
         requires std::movable<T>;
         requires std::derived_from<T, serializer>;
         {
@@ -52,6 +53,6 @@ namespace saucer
         } -> std::convertible_to<fmt::dynamic_format_arg_store<fmt::format_context>>;
         {
             T::resolve(std::declval<std::shared_ptr<std::promise<int>>>())
-        } -> std::convertible_to<serializer::promise>;
+        } -> std::convertible_to<serializer::resolver>;
     };
 } // namespace saucer
