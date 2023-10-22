d5db48dcc91bd275cca02d6d24082b20943b46fb
Curve <fynnbwdt@gmail.com>
2023-10-22 06:19:56 -0700

    feat(embedding-example): add unicode demonstration

--- examples/embedding/src/main.cpp
+++ examples/embedding/src/main.cpp
@@ -5,9 +5,16 @@ int main()
 {
     saucer::webview webview;
 
-    webview.set_size(500, 600);
-    webview.set_title("Automated Embedding!");
+    webview.set_title("Automated Embedding - 還有 Unicode！");
     webview.embed(saucer::embedded::all());
+    webview.set_size(500, 600);
+
+    webview.run_java_script(R"js(
+        const text = document.createElement('p');
+        text.innerText = "來自 C++ 和 Unicode 的問候！";
+
+        document.body.append(text);
+    )js");
 
     webview.serve("src/index.html");
     webview.set_dev_tools(true);
