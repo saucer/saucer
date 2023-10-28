153ed8d0d71b095e6a8f5426e2c289742aa6dc88
Curve <fynnbwdt@gmail.com>
2023-10-28 15:33:59 +0200

    fix(native): add webview2 header

---
+++ include/saucer/modules/native/webview2.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <wrl.h>
+#include <windows.h>
+#include <WebView2.h>
+
+namespace saucer::native
+{
+    struct window
+    {
+        HWND hwnd;
+    };
+
+    struct webview
+    {
+        Microsoft::WRL::ComPtr<ICoreWebView2> web_view;
+        Microsoft::WRL::ComPtr<ICoreWebView2Controller> controller;
+    };
+} // namespace saucer::native
