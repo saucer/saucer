cb8fd64f55619a9517be4fe4c20a95db19e994aa
Curve <fynnbwdt@gmail.com>
2024-03-26 14:25:31 +0100

    fix(bindings): embed

--- bindings/include/saucer/embed.h
+++ bindings/include/saucer/embed.h
@@ -2,8 +2,10 @@
 
 #ifdef __cplusplus
 #include <cstdint>
+#include <cstddef>
 #else
 #include <stdint.h>
+#include <stddef.h>
 #endif
 
 #ifdef __cplusplus
@@ -16,7 +18,8 @@ extern "C"
     saucer_embedded_files *saucer_embedded_files_new();
     void saucer_embedded_files_free(saucer_embedded_files *);
 
-    void saucer_embedded_files_add(saucer_embedded_files *, const char *file, const char *mime, const uint8_t *content);
+    void saucer_embedded_files_add(saucer_embedded_files *, const char *file, const char *mime, const uint8_t *content,
+                                   size_t content_size);
 
 #ifdef __cplusplus
 }
--- bindings/src/embed.cpp
+++ bindings/src/embed.cpp
@@ -1,27 +1,24 @@
 #include "embed.h"
 
+#include "utils.hpp"
 #include <saucer/webview.hpp>
 
 extern "C"
 {
-    struct saucer_embedded_files
-    {
-        saucer::webview::embedded_files files;
-    };
-
     saucer_embedded_files *saucer_embedded_files_new()
     {
-        return new saucer_embedded_files;
+        return cast(new saucer::webview::embedded_files);
     }
 
     void saucer_embedded_files_free(saucer_embedded_files *handle)
     {
-        delete handle;
+        delete cast(handle);
     }
 
     void saucer_embedded_files_add(saucer_embedded_files *handle, const char *file, const char *mime,
-                                   const uint8_t *content)
+                                   const uint8_t *content, size_t content_size)
     {
-        handle->files.emplace(file, mime, content);
+        auto item = saucer::embedded_file{mime, {content, content_size}};
+        cast(handle)->emplace(file, std::move(item));
     }
 }
