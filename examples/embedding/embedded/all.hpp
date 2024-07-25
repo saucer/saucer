21a88821a47698f16907088e9229d40f645359f5
redacted <redacted>
2024-07-25 06:08:25 -0700

    fix(examples): revert

--- examples/embedding/embedded/all.hpp
+++ examples/embedding/embedded/all.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <map>
 #include <string>
 #include <saucer/webview.hpp>
@@ -15,10 +14,10 @@ namespace saucer::embedded
     {
         std::map<std::string, embedded_file> rtn;
 
-        rtn.emplace("assets/logo.png", embedded_file{"image/png", stash<const std::uint8_t>::view(assets_logo_png)});
-        rtn.emplace("src/index.html", embedded_file{"text/html", stash<const std::uint8_t>::view(src_index_html)});
-        rtn.emplace("src/index.js", embedded_file{"application/javascript", stash<const std::uint8_t>::view(src_index_js)});
-        rtn.emplace("style/style.css", embedded_file{"text/css", stash<const std::uint8_t>::view(style_style_css)});
+        rtn.emplace("assets/logo.png", embedded_file{stash<>::view(assets_logo_png), "image/png"});
+        rtn.emplace("src/index.html", embedded_file{stash<>::view(src_index_html), "text/html"});
+        rtn.emplace("src/index.js", embedded_file{stash<>::view(src_index_js), "application/javascript"});
+        rtn.emplace("style/style.css", embedded_file{stash<>::view(style_style_css), "text/css"});
 
         return rtn;
     }
--- examples/json-bridge/main.cpp
+++ examples/json-bridge/main.cpp
@@ -12,18 +12,12 @@ struct custom_data
 auto another_func(const custom_data &data)
 {
     std::cout << "Called from thread: " << std::this_thread::get_id() << std::endl;
-    std::cout << "Sleeping 5 seconds..." << std::endl;
-
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-
     return custom_data{data.field * 2};
 }
 
 int main()
 {
-    using saucer::launch;
     using saucer::then;
-
     saucer::smartview webview;
 
     webview.set_title("Hello World!");
@@ -32,10 +26,38 @@ int main()
     webview.set_min_size(400, 500);
     webview.set_max_size(1000, 1200);
 
-    webview.expose("close_sync", [&]() { webview.close(); });
-    webview.expose("close_async", [&]() { webview.close(); }, saucer::launch::async);
+    webview.on<saucer::web_event::url_changed>([](const auto &url) { //
+        std::cout << "New url:" << url << std::endl;
+    });
 
-    webview.set_url("https://github.com");
+    webview.on<saucer::window_event::resize>([](auto width, auto height) { //
+        std::cout << width << ":" << height << std::endl;
+    });
+
+    webview.evaluate<float>("Math.random()") | then([](auto result) { //
+        std::cout << "Random: " << result << std::endl;
+    });
+
+    webview.evaluate<int>("Math.pow({},{})", 5, 2) | then([](auto result) { //
+        std::cout << "Pow(5,2): " << result << std::endl;
+    });
+
+    webview.expose("some_func", [](int param) { return param * 10; });
+
+    webview.evaluate<int>("await window.saucer.call({})", saucer::make_args("some_func", std::make_tuple(10))) |
+        then([](auto result) { //
+            std::cout << "some_func: " << result << std::endl;
+        });
+
+    std::cout << "Main thread: " << std::this_thread::get_id() << std::endl;
+    webview.expose("another_func", another_func, saucer::launch::async);
+
+    webview.evaluate<custom_data>("await window.saucer.call({}, [{}])", "another_func", custom_data{500}) |
+        then([](auto result) { //
+            std::cout << "another_func: " << result.field << std::endl;
+        });
+
+    webview.set_url("https://github.com/saucer/saucer");
     webview.set_dev_tools(true);
 
     webview.show();
