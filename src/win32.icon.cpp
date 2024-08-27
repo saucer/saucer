f852e0b7a8a215ac49e48a3b2bc506cb88257f0a
redacted <redacted>
2024-08-27 08:43:39 -0700

    feat(icon/win32): implement `save`

--- src/win32.icon.cpp
+++ src/win32.icon.cpp
@@ -1,5 +1,7 @@
 #include "win32.icon.impl.hpp"
 
+#include "win32.utils.hpp"
+
 #include <shlwapi.h>
 
 namespace saucer
@@ -37,6 +39,19 @@ namespace saucer
     bool icon::empty() const
     {
         return !m_impl->bitmap || m_impl->bitmap->GetLastStatus() != Gdiplus::Status::Ok;
+    }
+
+    void icon::save(const fs::path &path) const
+    {
+        if (!m_impl->bitmap)
+        {
+            return;
+        }
+
+        // https://stackoverflow.com/questions/5345803/does-gdi-have-standard-image-encoder-clsids
+        static const CLSID encoder = {0x557cf406, 0x1a04, 0x11d3, {0x9a, 0x73, 0x00, 0x00, 0xf8, 0x1e, 0xf3, 0x2e}};
+
+        m_impl->bitmap->Save(utils::widen(path.string()).c_str(), &encoder, nullptr);
     }
 
     std::optional<icon> icon::from(const stash<> &ico)
