667391e8fd0f516803cfa8a8d9673c4d5ff2b35f
Curve <fynnbwdt@gmail.com>
2024-09-26 13:20:32 +0200

    feat(macos): add `desktop::open` [skip ci]

---
+++ src/cocoa.desktop.mm
@@ -0,0 +1,19 @@
+#include "desktop.hpp"
+
+#include <filesystem>
+
+#import <Cocoa/Cocoa.h>
+
+namespace saucer
+{
+    namespace fs = std::filesystem;
+
+    void desktop::open(const std::string &uri)
+    {
+        auto *const workspace = [NSWorkspace sharedWorkspace];
+        auto *const str       = [NSString stringWithUTF8String:uri.c_str()];
+        auto *const url       = fs::exists(uri) ? [NSURL fileURLWithPath:str] : [NSURL URLWithString:str];
+
+        [workspace openURL:url];
+    }
+} // namespace saucer
