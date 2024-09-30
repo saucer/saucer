253cfb7c56e1fcea4469d50b75779adce0fda6f0
Curve <fynnbwdt@gmail.com>
2024-10-01 01:05:18 +0200

    fix(macos): build issues with non xcode clang
    The jthread implementation provided by StirlingLabs caused issues with clang > 15 where some symbols were partially defined. Moved to in-house fork where these issues are addressed.

---
+++ polyfill/jthread.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <nonstd/jthread.hpp>
+
+// NOLINTBEGIN(*-dcl58-cpp)
+namespace std
+{
+    using nonstd::jthread;
+    using nonstd::stop_source;
+    using nonstd::stop_token;
+} // namespace std
+// NOLINTEND(*-dcl58-cpp)
