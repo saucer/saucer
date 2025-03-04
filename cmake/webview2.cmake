0d0a74ac97fcf78772322757945a34970f31f627
Curve <fynnbwdt@gmail.com>
2025-03-04 19:48:47 +0100

    refactor(cmake/winndows): allow to set arch for nuget packages

--- cmake/webview2.cmake
+++ cmake/webview2.cmake
@@ -1,5 +1,17 @@
 include("cmake/nuget.cmake")
 
+if (saucer_windows_arch STREQUAL "Default")
+    if (CMAKE_GENERATOR_PLATFORM)
+        set(saucer_windows_arch "${CMAKE_GENERATOR_PLATFORM}")
+    elseif (CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(saucer_windows_arch "x64")
+    else()
+        set(saucer_windows_arch "Win32")
+    endif()
+
+    message(STATUS "[saucer] Windows Arch is 'Default', using ${saucer_windows_arch}")
+endif()
+
 if (NOT saucer_prefer_remote)
     find_package(webview2 CONFIG REQUIRED)
     return()
@@ -11,10 +23,12 @@ if (NOT TARGET webview2::webview2)
     add_library(webview2 STATIC IMPORTED)
     add_library(webview2::webview2 ALIAS webview2)
 
-    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-          set(webview2_lib_path "${WebView2_PATH}/build/native/x64/WebView2LoaderStatic.lib")
+    if (saucer_windows_arch STREQUAL "ARM64")
+        set(webview2_lib_path "${WebView2_PATH}/build/native/arm64/WebView2LoaderStatic.lib")
+    elseif (saucer_windows_arch STREQUAL "x64")
+        set(webview2_lib_path "${WebView2_PATH}/build/native/x64/WebView2LoaderStatic.lib")
     else()
-          set(webview2_lib_path "${WebView2_PATH}/build/native/x86/WebView2LoaderStatic.lib")
+        set(webview2_lib_path "${WebView2_PATH}/build/native/x86/WebView2LoaderStatic.lib")
     endif()
 
     set_target_properties(webview2 PROPERTIES
