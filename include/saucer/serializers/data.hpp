2fda99ae16ebe7ea51d28b949db38f3cbf908b59
Curve <fynnbwdt@gmail.com>
2025-05-12 19:57:02 +0200

    fix(serializer/data): add virtual destructors

--- include/saucer/serializers/data.hpp
+++ include/saucer/serializers/data.hpp
@@ -12,11 +12,17 @@ namespace saucer
     {
         std::uint64_t id;
         std::string name;
+
+      public:
+        virtual ~function_data() = default;
     };
 
     struct result_data
     {
         std::uint64_t id;
+
+      public:
+        virtual ~result_data() = default;
     };
 
     using message_data = std::variant<std::unique_ptr<function_data>, std::unique_ptr<result_data>, std::monostate>;
