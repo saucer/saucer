d37f5ffd3e6a2ecd6d7292c98ed362a4744a0077
Curve <fynnbwdt@gmail.com>
2025-07-02 14:51:06 +0200

    refactor(permission/wv2): return `uri`

--- src/wv2.permission.cpp
+++ src/wv2.permission.cpp
@@ -12,12 +12,11 @@ namespace saucer::permission
 
     request::~request() = default;
 
-    std::string request::url() const
+    uri request::url() const
     {
         utils::string_handle raw;
         m_impl->request->get_Uri(&raw.reset());
-
-        return utils::narrow(raw.get());
+        return uri::parse(utils::narrow(raw.get())).value_or({});
     }
 
     permission::type request::type() const
