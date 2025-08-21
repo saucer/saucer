d8c85ac6dca24ef9333f70305dd1ced3c45e6348
Curve <fynnbwdt@gmail.com>
2025-08-21 15:30:53 +0200

    refactor(cmake): restrict module header access [skip ci]

--- cmake/module.cmake
+++ cmake/module.cmake
@@ -1,12 +1,11 @@
 function(saucer_add_module NAME)
     message(STATUS "[saucer] Adding module: ${NAME}")
     
-    get_target_property(priv_includes saucer::saucer INCLUDE_DIRECTORIES)
-
     target_link_libraries(${NAME} PUBLIC saucer::saucer)
-    target_include_directories(${NAME} PRIVATE ${priv_includes})
+    target_include_directories(${PROJECT_NAME} PRIVATE "${_saucer_root}/private")
 
     set(saucer_backend ${_saucer_backend} PARENT_SCOPE)
 endfunction()
 
 set(_saucer_backend ${saucer_backend} CACHE INTERNAL "")
+set(_saucer_root ${CMAKE_CURRENT_SOURCE_DIR} CACHE INTERNAL "")
