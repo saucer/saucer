6bb46de3a4f466d5a99ad85722786c881cb00442
Curve <fynnbwdt@gmail.com>
2025-09-15 23:57:17 +0200

    refactor(cmake): remove `saucer_modules` option

--- cmake/module.cmake
+++ cmake/module.cmake
@@ -1,11 +1,18 @@
-function(saucer_add_module NAME)
+macro(saucer_link_libraries NAME)
+    list(APPEND saucer_link_targets ${ARGN})
+    target_link_libraries(${NAME} PRIVATE ${ARGN})
+    set(_saucer_link_targets ${saucer_link_targets} CACHE INTERNAL "")
+endmacro()
+
+macro(saucer_add_module NAME)
     message(STATUS "[saucer] Adding module: ${NAME}")
-    
-    target_link_libraries(${NAME} PUBLIC saucer::saucer)
+
+    target_link_libraries(${NAME}              PUBLIC  saucer::saucer)
+    target_link_libraries(${NAME}              PRIVATE ${_saucer_link_targets})
     target_include_directories(${PROJECT_NAME} PRIVATE "${_saucer_root}/private")
 
-    set(saucer_backend ${_saucer_backend} PARENT_SCOPE)
-endfunction()
+    set(saucer_backend ${_saucer_backend})
+endmacro()
 
 set(_saucer_backend ${saucer_backend} CACHE INTERNAL "")
 set(_saucer_root ${CMAKE_CURRENT_SOURCE_DIR} CACHE INTERNAL "")
