c01e97a5a1e93b9214677f5673eaf7912637935d
Curve <fynnbwdt@gmail.com>
2025-09-16 00:04:54 +0200

    fix(cmake/module): add meta target

--- cmake/module.cmake
+++ cmake/module.cmake
@@ -1,8 +1,20 @@
+# --------------------------------------------------------------------------------------------------------
+# Setup Meta-Library
+# └ Modules can then link against this target to get access to all internal dependencies.
+# --------------------------------------------------------------------------------------------------------
+
+add_library(saucer_private  INTERFACE)
+add_library(saucer::private ALIAS saucer_private)
+
 macro(saucer_link_libraries NAME)
-    list(APPEND saucer_link_targets ${ARGN})
     target_link_libraries(${NAME} PRIVATE ${ARGN})
-    set(_saucer_link_targets ${saucer_link_targets} CACHE INTERNAL "")
+    target_link_libraries(saucer_private INTERFACE ${ARGN})
 endmacro()
+
+# --------------------------------------------------------------------------------------------------------
+# Setup Modules
+# └ Defines functions that are meant to be used from modules.
+# --------------------------------------------------------------------------------------------------------
 
 macro(saucer_add_module NAME)
     message(STATUS "[saucer] Adding module: ${NAME}")
