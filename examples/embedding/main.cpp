6d732c3b5abcb7244fcd7a8a99e75087d9678c72
Curve <fynnbwdt@gmail.com>
2025-09-22 13:38:59 +0200

    feat(examples): add embedding example

---
+++ examples/embedding/main.cpp
@@ -0,0 +1,21 @@
+#include <saucer/smartview.hpp>
+#include <saucer/embedded/all.hpp>
+
+coco::stray start(saucer::application *app)
+{
+    auto window  = saucer::window::create(app).value();
+    auto webview = saucer::smartview<>::create({.window = window});
+
+    webview->embed(saucer::embedded::all());
+    webview->set_dev_tools(true);
+
+    webview->serve("/src/index.html");
+    window->show();
+
+    co_await app->finish();
+}
+
+int main()
+{
+    return saucer::application::create({.id = "embedding"})->run(start);
+}
