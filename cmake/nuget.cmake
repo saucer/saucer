a9fb4d6b6bea62ce0609b2da8a7b5989f9c53e36
Curve <fynnbwdt@gmail.com>
2025-09-22 13:37:39 +0200

    refactor(cmake/nuget): clean up

--- cmake/nuget.cmake
+++ cmake/nuget.cmake
@@ -11,32 +11,30 @@ function(nuget_add OUTPUT_NAME NAME VERSION)
 
         set(NUGET "${CMAKE_CURRENT_BINARY_DIR}/nuget/nuget.exe")
     endif()
-    
-    execute_process(COMMAND 
-        ${NUGET} help 
+
+    execute_process(
+        COMMAND ${NUGET} help
         OUTPUT_VARIABLE NUGET_INFO
     )
-    
+
     string(REGEX MATCH "NuGet Version: ([0-9.]+)" _ "${NUGET_INFO}")
     message(STATUS "[NuGet] Found at: ${NUGET} [${CMAKE_MATCH_1}]")
 
-    execute_process(COMMAND 
-        ${NUGET} sources 
+    execute_process(
+        COMMAND ${NUGET} sources
         OUTPUT_VARIABLE NUGET_SOURCES
     )
-    
+
     if (NOT "${NUGET_SOURCES}" MATCHES "https:\/\/api\.nuget\.org\/v3\/index\.json")
         message(WARNING "[NuGet] Official NuGet Sources missing!")
-        
-        execute_process(COMMAND 
-            ${NUGET} sources Add -Name "nuget.org" -Source "https://api.nuget.org/v3/index.json"
-        )
+        execute_process(COMMAND ${NUGET} sources Add -Name "nuget.org" -Source "https://api.nuget.org/v3/index.json")
     endif()
 
-    execute_process(COMMAND 
-        ${NUGET} install ${NAME} -Version ${VERSION} -ExcludeVersion -OutputDirectory "${CMAKE_CURRENT_BINARY_DIR}/nuget" 
-        OUTPUT_QUIET COMMAND_ERROR_IS_FATAL ANY
+    execute_process(
+        COMMAND ${NUGET} install ${NAME} -Version ${VERSION} -ExcludeVersion -OutputDirectory "${CMAKE_CURRENT_BINARY_DIR}/nuget"
+        COMMAND_ERROR_IS_FATAL ANY
+        OUTPUT_QUIET
     )
-    
+
     set(${OUTPUT_NAME}_PATH "${CMAKE_CURRENT_BINARY_DIR}/nuget/${NAME}" PARENT_SCOPE)
 endfunction()
